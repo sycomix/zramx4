397c19c52e9b7525eaf22b9821f6bf10766fab1a
sycomix <6117352+sycomix@users.noreply.github.com>
2018-07-27 20:24:59 -0500

    Create zramx4.sh

---
+++ zramx4.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          zram
+# Required-Start:    $local_fs
+# Required-Stop:     $local_fs
+# Default-Start:     S
+# Default-Stop:      0 1 6
+# Short-Description: Use compressed RAM as in-memory swap
+# Description:       Use compressed RAM as in-memory swap
+### END INIT INFO
+
+# Adjust the swap size in MB
+SWAP_SIZE=800
+
+CPUS=`nproc`
+SIZE=$(( SWAP_SIZE * 1024 * 1024 / CPUS ))
+
+case "$1" in
+  "start")
+    # if a zram swap already exists, bail out
+    if [ `grep -c zram /proc/swaps` != 0 ]
+      then
+      echo "There is already a zram swap"
+      exit 1
+    fi
+    modprobe zram
+
+    for n in `seq $CPUS`; do
+      i=`cat /sys/class/zram-control/hot_add`
+      echo $SIZE > /sys/block/zram$i/disksize
+      mkswap /dev/zram$i
+      swapon /dev/zram$i -p 10
+    done
+    ;;
+  "stop")
+    readarray arr < "/proc/swaps"
+    for line in "${arr[@]}" ; do
+      if [ ${line:0:9} == "/dev/zram" ] ; then
+          i=${line:9:1}
+          echo "Removing swap zram $i"
+          swapoff /dev/zram$i
+          echo $i > /sys/class/zram-control/hot_remove
+      fi
+    done
+    modprobe -r zram
+    ;;
+  "status")
+    free -h
+    cat /proc/swaps
+    ;;
+  *)
+    echo "Usage: `basename $0` (start | stop | status)"
+    exit 1
+    ;;
+esac
